24b1d6d13f16652ebd2273499a5225b465f14156
= <=>
2025-09-18 11:33:53 -0300

    Arreglando el naming

---
+++ client.py
@@ -0,0 +1,12 @@
+from promptLLM import OpenAIClient
+
+url_api_ollama = "http://localhost:11434/api/generate"
+
+data = {
+    "model": "gpt-oss:20b",
+    "prompt": "Write a four-stanza poem about spring in the Eastern Republic of Uruguay. In English and Spanish.",
+}
+
+client = OpenAIClient(base_url=url_api_ollama)
+
+client.call(data)
--- promptLLM.py
+++ promptLLM.py
@@ -30,16 +30,5 @@ class OpenAIClient:
                 print(obj["response"], end="")
         print()
 
-url_api_ollama = "http://localhost:11434/api/generate"
-
-data = {
-    "model": "gpt-oss:20b",
-    "prompt": "Write a four-stanza poem about spring in the Eastern Republic of Uruguay. In English and Spanish.",
-}
-
-client = OpenAIClient(base_url=url_api_ollama)
-
-client.call(data)
-
 
 
